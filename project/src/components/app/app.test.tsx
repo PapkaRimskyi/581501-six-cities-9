17c4fe9b55c58b3769a4308e28e64baa3ab60ba2
Георгий Гуров <gurov@mos-team.ru>
2022-02-13 16:04:07 +0300

    templates for each pages

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,8 +1,10 @@
 import {render, screen} from '@testing-library/react';
 import App from './app';
 
+const ARENDA_OFFER_DATA = [0, 1, 2, 3, 4];
+
 test('Renders app-component', () => {
-  render(<App />);
+  render(<App arendaOfferData={ARENDA_OFFER_DATA} />);
   const textElement = screen.getByText(/Hello, world!/i);
   expect(textElement).toBeInTheDocument();
 });
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/app/app';
+
+const ARENDA_OFFER_DATA = [0, 1, 2, 3, 4];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App arendaOfferData={ARENDA_OFFER_DATA} />
   </React.StrictMode>,
   document.getElementById('root'));
