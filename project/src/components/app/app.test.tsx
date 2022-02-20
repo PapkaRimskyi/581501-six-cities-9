bc4e7529d8ce0d72f226aadd8e7e5843daadfcbe
Георгий Гуров <gurov@mos-team.ru>
2022-02-20 14:42:57 +0300

    fix test

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import {render, screen} from '@testing-library/react';
 import App from './app';
 
 const ARENDA_OFFER_DATA = [0, 1, 2, 3, 4];
 
 test('Renders app-component', () => {
-  render(<App arendaOfferData={ARENDA_OFFER_DATA} />);
+  render(<Router><App arendaOfferData={ARENDA_OFFER_DATA} /></Router>);
   const textElement = screen.getByText(/5 places to stay in Amsterdam/i);
   expect(textElement).toBeInTheDocument();
 });
